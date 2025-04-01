67f54128d72dd4a8eafa3bf6a65326faf2ad44d9
Ishav <ishavlega@gmail.com>
2025-04-01 11:38:38 +0530

    init

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`App is listening to port ${PORT}`);
+});
---
+++ apps/web/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+  return (
+    <div className="font-bold text-2xl">
+      hello
+    </div>
+  );
+}
